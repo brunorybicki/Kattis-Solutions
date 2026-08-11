02ed3e30d595f8b1a20df7525a037d823ce8dc1b
Bruno Rybicki <175746513+brunorybicki@users.noreply.github.com>
2026-08-12 01:40:46 +0200

    Add solutions for Bitte ein Bit

---
+++ Bitte ein Bit/bitteeinbit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char bit;
+    scanf("%c", &bit);
+
+    printf("%c", bit);
+    return 0;
+}
---
+++ Bitte ein Bit/bitteeinbit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class bitteeinbit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char bit = scanner.next().charAt(0);
+        System.out.println(bit);
+    }
+}
